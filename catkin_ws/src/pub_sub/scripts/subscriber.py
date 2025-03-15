7b61ca07d92365bfcb8b689c638fa889fb4f321f
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-03-16 01:02:31 +0530

    Updated subscriber.py file

---
+++ catkin_ws/src/pub_sub/scripts/subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int64
+
+# Callback function
+def callback(msg):
+    print(msg)
+
+# Node + Subscriber initialization
+rospy.init_node("subscriber_node")
+rospy.Subscriber("topic", Int64, callback)
+
+# Spinning
+rospy.spin()
