5e880c8686b72308eac364ae91d9d32260203ab9
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-03-31 09:32:12 +0530

    Created Subscriber function in control.py File

--- catkin_ws/src/turtle_play/scripts/control.py
+++ catkin_ws/src/turtle_play/scripts/control.py
@@ -2,12 +2,17 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+
+def callback(msg):
+    print(msg)
 
 rospy.init_node("control")
 pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size = 1)
+rospy.Subscriber("/turtle1/pose", Pose, callback)
 
 msg = Twist()
-msg.linear.x = -1
+msg.angular.z = 1
 
 """msg.linear.y = 2
 msg.linear.z = 3
@@ -23,3 +28,4 @@ for i in range(5):
     pub.publish(msg)
     rospy.sleep(1)
 
+#rospy.spin()
