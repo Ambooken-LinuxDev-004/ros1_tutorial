f37b54fa65244dd6e6ad0f4c7892042ae7b08aed
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-04-12 17:41:36 +0530

    Tried to display String message using Publisher & Subscriber but didn't work

--- catkin_ws/src/pub_sub/scripts/publisher.py
+++ catkin_ws/src/pub_sub/scripts/publisher.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
+"""
 import rospy
 from std_msgs.msg import Int64
 
@@ -11,4 +12,18 @@ pub = rospy.Publisher("topic", Int64, queue_size=1)
 # Looping
 while not rospy.is_shutdown():
     pub.publish(1)
+    rospy.sleep(1)
+
+"""
+
+import rospy
+from std_msgs.msg import String
+
+rospy.init_node("string_publisher_node")
+pub = rospy.Publisher("string_topic", String, queue_size=10)
+msg = String()
+msg.data = "Joe"
+
+while rospy.is_shutdown():
+    pub.publish(msg.data)
     rospy.sleep(1)
--- catkin_ws/src/pub_sub/scripts/subscriber.py
+++ catkin_ws/src/pub_sub/scripts/subscriber.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+"""
 import rospy
 from std_msgs.msg import Int64
 
@@ -12,4 +13,17 @@ rospy.init_node("subscriber_node")
 rospy.Subscriber("topic", Int64, callback)
 
 # Spinning
+rospy.spin()
+
+"""
+
+import rospy
+from std_msgs.msg import String
+
+def reply(msg):
+    print("Hello " + msg + ", Welcome to ROS!!...")
+
+rospy.init_node("string_subscriber_node")
+rospy.Subscriber("string_topic", String, reply)
+
 rospy.spin()
